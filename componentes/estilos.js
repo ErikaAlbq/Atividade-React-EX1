434f538daebdb52641b60a5aea97fdfc277f63c4
ErikaAlbq <erica.albuquerque@fatec.sp.gov.br>
2020-05-25 17:56:52 -0300

    1 file modified, 1 added, 2 deleted

--- componentes/estilos.js
+++ componentes/estilos.js
@@ -13,30 +13,19 @@ const estilos = {
     flexDirection: 'row',
     justifyContent: 'center',
   },
-  botao_ela: {
-    backgroundColor: "#8B0000",
-    padding: 10,
-    marginRight: 10,
+  botao_pessoas: {
+    backgroundColor: "#303BD3",
+    padding: 20,
+    marginRight: 25,
     marginLeft: 10,
     borderWidth: 1,
     borderRadius: 5,
     color: "#000",
     fontWeigth: 'bold',
     fontFamily: 'Comic Sans MS',
-    fontSize: 20
-  },
- botao_ele: {
-    backgroundColor: "#0000CD",
-    padding: 10,
-    marginRight: 10,
-    marginLeft: 10,
-    borderWidth: 1,
-    borderRadius: 5,
-    color: "#000",
-    fontWeigth: 'bold',
-    fontFamily: 'Comic Sans MS',
-    fontSize: 20
+    fontSize: 2
   }
+ 
 
 
 
--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-import Ela from './componentes/Ela';
-import Ele from './componentes/Ele';
 import estilos from "./componentes/estilos";
-
+import Pessoa from "./componentes/Pessoa";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      q_clicou: 'Ninguem'
+      q_clicou: 'Ninguem',
+      
     };
   }
 
  alterar = (origem)=>{
    this.setState({
-     q_clicou:origem
+     q_clicou: origem
    });
+ 
  }
 
-
-
-
+ 
   render() {
+    
     return (
    <div style={estilos.container}>
-         <p style={estilos.titulo}>Quem clicou? {this.state.q_clicou}</p>
-         <div style={estilos.linha}>
-            <Ela alterar={this.alterar} />
-            <Ele alterar={this.alterar} />
-       </div>
-      </div>
+         <p  style={estilos.titulo}>Quem clicou? {this.state.q_clicou}</p>
+         
+         <div style={estilos.linha }>
+         
+            <Pessoa pessoa= "ELA" alterar={this.alterar}  />
+            <Pessoa pessoa= "ELE" alterar={this.alterar}  />
+        
+          </div>
+        </div>
+      
     );
   }
 }
